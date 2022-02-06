e63b0921357822ebe0c3e5072d4b4d6d132d3baf
Gregg <homespan@icloud.com>
2022-02-06 16:40:50 -0600

    Enabled OTA and set Sketch Version to 2.0

--- ZephyrVentHood.ino
+++ ZephyrVentHood.ino
@@ -4,16 +4,20 @@
 #include "DEV_Identify.h"
 #include "DEV_Zephyr.h"
 
+#define SKETCH_VERSION  "2.0"
+
 void setup() {
 
   Serial.begin(115200);
 
   homeSpan.setLogLevel(1);
-
+  homeSpan.enableOTA();
+  homeSpan.setSketchVersion(SKETCH_VERSION);
+  
   homeSpan.begin(Category::Fans,"Zephyr Vent Hood");
 
   new SpanAccessory();
-    new DEV_Identify("Vent Hood","HomeSpan","ZVH-1","RF-Control","1.0.1",0);
+    new DEV_Identify("Vent Hood","HomeSpan","ZVH-1","RF-Control",SKETCH_VERSION,0);
     new Service::HAPProtocolInformation();
       new Characteristic::Version("1.1.0");
     (new DEV_ZephyrFan(0x51388,0x61398,18))->setPrimary();
